22374c7002aea76cb5c16a1c5118724b6a810072
ajheaps <a.j.heaps@reading.ac.uk>
2014-01-17 13:25:55 +0000

    Hovmuller commit

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="cfplot",
-    version="1.1",
+    version="1.2",
     author="Andy Heaps",
     author_email="a.j.heaps@reading.ac.uk",
     packages=['cfplot'],
