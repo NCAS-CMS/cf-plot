40457bd75d95a082fc535315e964ec42c1e40ab0
ajheaps <a.j.heaps@reading.ac.uk>
2014-03-17 15:38:44 +0000

    missing __init__.py file

---
+++ cfplot/__init__.py
@@ -0,0 +1,19 @@
+'''
+
+cfplot is a set of Python plotting routines for contour/vector plots that
+climate researchers commonly make.
+
+
+See the cfplot home page http://cms.ncas.ac.uk/~andy/cfplot for downloads, installation
+and source code.
+
+
+'''
+
+__author__ = 'Andy Heaps - a.j.heaps@reading.ac.uk'
+__date__ = '17 March 2014'
+__version__ = '1.4'
+
+from cfplot import *
+
+
