a095f8ef2ab271116b5e2b03095b6483d689cb39
ajheaps <a.j.heaps@reading.ac.uk>
2015-11-30 11:45:13 +0000

    web page link in __init__.py corrected

--- __init__.py
+++ __init__.py
@@ -4,15 +4,14 @@ cfplot is a set of Python plotting routines for contour/vector plots that
 climate researchers commonly make.
 
 
-See the cfplot home page http://cms.ncas.ac.uk/~andy/cfplot for downloads, installation
-and source code.
+See the cfplot home page http://climate.ncas.ac.uk/~andy/cfplot_sphinx/_build/html/# for a gallery of plots and how to use cfplot.
 
 
 '''
 
 __author__ = 'Andy Heaps - a.j.heaps@reading.ac.uk'
-__date__ = '77th November 2015'
-__version__ = '1.7.21'
+__date__ = '30th November 2015'
+__version__ = '1.7.22'
 
 from cfplot import *
 
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 setup(
     name="cfplot",
-    version="1.7.21",
+    version="1.7.22",
     author="Andy Heaps",
     author_email="a.j.heaps@reading.ac.uk",
     packages=['cfplot'],
