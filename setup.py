a0cb5be5947d562e11e79c6e2b06c0e47d72fa55
ajheaps <a.j.heaps@reading.ac.uk>
2015-12-01 11:38:03 +0000

    setup.py changes

--- setup.py
+++ setup.py
@@ -3,17 +3,20 @@ try:
 except ImportError:
     from distutils.core import setup
 
+
+
 setup(
     name="cfplot",
-    version="1.7.23",
+    version="1.7.24",
     author="Andy Heaps",
     author_email="a.j.heaps@reading.ac.uk",
     packages=['cfplot'],
     package_dir={'cfplot':'cfplot'},
-    package_data={'cfplot': ['colourmaps/*']},
-    include_package_data=True
+    package_data={'cfplot': 'colourmaps/*'},
+    include_package_data=True,
     url="http://climate.ncas.ac.uk/~andy/cfplot_sphinx/_build/html",
     license='LICENSE.txt',
     description="Climate contour and vector plots in Python",
     long_description=open('README.txt').read(),
+    zip_safe=False
 )
