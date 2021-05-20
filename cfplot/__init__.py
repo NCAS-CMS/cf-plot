ca0c36cb75f50e711e3b2b1943c13562ec1dc17d
ajheaps <andy.heaps@ncas.ac.uk>
2021-05-20 12:06:14 +0100

    stray character update

--- cfplot/__init__.py
+++ cfplot/__init__.py
@@ -11,7 +11,7 @@ See the cf-plot home page http://ajheaps.github.io/cf-plot for a gallery of plot
 
 __author__ = 'Andy Heaps - andy.heaps@ncas.ac.uk'
 __date__ = '20th May 2021'
-__version__ = '3.1.3'
+__version__ = '3.1.4'
 
 from .cfplot import *
 
