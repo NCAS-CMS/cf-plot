e07ff917f0f1306db9e12b813b6c0e9559fb9abd
Sadie L. Bartholomew <sadie.bartholomew@ncas.ac.uk>
2024-04-03 15:42:37 +0100

    Apply configured 'black' to whole cfplot/ directory

--- cfplot/__init__.py
+++ cfplot/__init__.py
@@ -1,4 +1,4 @@
-'''
+"""
 
 cf-plot is a set of Python plotting routines for the contour, vector and line plots that
 climate researchers commonly make.
@@ -7,12 +7,10 @@ climate researchers commonly make.
 See the cf-plot home page http://ajheaps.github.io/cf-plot for a gallery of plots and how to use cf-plot.
 
 
-'''
+"""
 
-__author__ = 'Andy Heaps - andy.heaps@ncas.ac.uk'
-__date__ = '15th Jan 2024'
-__version__ = '3.3.0'
+__author__ = "Andy Heaps - andy.heaps@ncas.ac.uk"
+__date__ = "15th Jan 2024"
+__version__ = "3.3.0"
 
 from .cfplot import *
-
-
