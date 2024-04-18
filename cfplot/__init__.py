38847615ed736601da3c69a3865fd1708746730f
Sadie L. Bartholomew <sadie.bartholomew@ncas.ac.uk>
2024-04-18 11:22:58 +0100

    Fix or ignore w/ justification PEP8 errors in __init.py__

--- cfplot/__init__.py
+++ cfplot/__init__.py
@@ -1,16 +1,13 @@
 """
+cf-plot is a set of Python plotting routines for the contour, vector
+and line plots that climate researchers commonly make.
 
-cf-plot is a set of Python plotting routines for the contour, vector and line plots that
-climate researchers commonly make.
-
-
-See the cf-plot home page http://ajheaps.github.io/cf-plot for a gallery of plots and how to use cf-plot.
-
-
+See the cf-plot home page http://ajheaps.github.io/cf-plot for a
+gallery of plots and how to use cf-plot.
 """
 
 __author__ = "Andy Heaps - andy.heaps@ncas.ac.uk"
 __date__ = "15th Jan 2024"
 __version__ = "3.3.0"
 
-from .cfplot import *
+from .cfplot import *  # noqa
